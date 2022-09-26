9f5c1663eaefbf765531732c27e6665f9e24306a
kabanijHolmik <kosenkov809@gmail.com>
2022-09-26 15:11:37 +0300

    Nachalo

--- Lab2.cpp
+++ Lab2.cpp
@@ -16,77 +16,22 @@ private:
 	double re;
 	double im;
 public:
-	friend Complex& operator+(Complex& x, Complex& y) {
-		Complex* sum = new Complex();
-		sum->re = x.re + y.re;
-		sum->im = x.im + y.im;
-		return *sum;
+	Complex operator+(const Complex& x) {
+		Complex tmp;
+		tmp.re = this->re + x.re;
+		tmp.im = this->im + x.im;
+		return tmp;
 	}
-
-	void Init(double re, double im) {
-		this->re = re;
-		this->im = im;
+	void push() {
+		cout << this->re << this->im;
 	}
-	void Print(Complex* x) {
-		if (this->im < 0) cout << this->re << this->im << "i" << endl; 
-		else cout << this->re << "+" << this->im << "i" << endl;
-	}
-
-	void Add(Complex* x, Complex* y) {
-		double a, b;
-		a = x->re + y->re;
-		b = x->im + y->im;
-		result.Init(a, b);
-		Complex::Print(&result);
-	}
-
-	void Sub(Complex* x, Complex* y) {
-		double a, b;
-		a = x->re - y->re;
-		b = x->im - y->im;
-		result.Init(a, b);
-		Complex::Print(&result);
-	}
-
-	void Mul(Complex* x, Complex* y) {
-		double a, b;
-		a = x->re * y->re - x->im * y->im;
-		b = x->re * y->im + x->im * y->re;
-		result.Init(a, b);
-		Complex::Print(&result);
-	}
-
-	void Div(Complex* x, Complex* y) {
-		double a, b; 
-		a = (x->re * y->re + x->im * y->im) / (y->re * y->re + y->im * y->im);
-		b = (y->re * x->im - x->re * y->im) / (y->re * y->re + y->im * y->im);
-		result.Init(a, b);
-		Complex::Print(&result);
-	}
-}result, t;
+};
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	Complex* x = new Complex(1, 2);
-	Complex* y = new Complex(2, 1);
-	Complex* sum = new Complex();
-	*sum = *x + *y;
-	sum->Print(sum);
-	Complex* clone = new Complex(*x);
-	clone->Print(clone);
-	cout << "x = ";
-	x->Print(x);
-	cout << "y = ";
-	y->Print(y);
-	cout << "Результат сложения:";
-	result.Add(x, y);
-	cout << "Результат вычитания:";
-	result.Sub(x, y);
-	cout << "Результат умножения:";
-	result.Mul(x, y);
-	cout << "Результат деления:";
-	result.Div(x, y);
-	delete clone;
+	Complex t(1,2), tm, x(2,1);
+	tm = t + x;
+	tm.push();
 	return 0;
 }
